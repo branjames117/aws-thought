4657c09c6b533a7baefc36d92e4d709eeffd63d8
Brandon Hoskins <branjames117@gmail.com>
2022-03-31 22:30:42 -0500

    created api endpoints for querying and creating thoughts

---
+++ server/db/CreateThoughtsTable.js
@@ -0,0 +1,37 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  region: 'us-east-2',
+});
+
+const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+
+const params = {
+  TableName: 'Thoughts',
+  KeySchema: [
+    { AttributeName: 'username', KeyType: 'HASH' }, // partition key
+    { AttributeName: 'createdAt', KeyType: 'RANGE' }, // sort key
+  ],
+  AttributeDefinitions: [
+    { AttributeName: 'username', AttributeType: 'S' }, // string
+    { AttributeName: 'createdAt', AttributeType: 'N' }, // number
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 10,
+    WriteCapacityUnits: 10,
+  },
+};
+
+dynamodb.createTable(params, (err, data) => {
+  if (err) {
+    console.error(
+      'Unable to create table. Error JSON: ',
+      JSON.stringify(err, null, 2)
+    );
+  } else {
+    console.log(
+      'Created table. Table description JSON: ',
+      JSON.stringify(data, null, 2)
+    );
+  }
+});
